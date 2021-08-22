54dcc03ef63c1cc10c49bf5867995c2fcfdca766
austin <austinlee199723@yahoo.com>
2021-08-21 19:35:28 -0700

    added docs

--- commands.bat
+++ commands.bat
@@ -1,2 +1,2 @@
 
-npm run build &&  git add . && git commit -m "converted to a fully static html page" && git push origin pages
+npm run build &&  git add . && git commit -m "added docs" && git push origin pages
