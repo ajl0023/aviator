d72cb2fe935dc6fa8ad6635b1d7914794a90e692
austin <austinlee199723@yahoo.com>
2021-08-23 13:02:41 -0700

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
-  entry: "./docs/main.js",
+  entry: "./src/index.js",
 
   module: {
     rules: [
